996bc660bcb1647c905a245661f2a42eb6f68ff9
Johan <jsnider@users.noreply.github.com>
2020-09-21 14:23:11 +0200

    increased live to 5

--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     mouse.move(1)
 })
-let mouse : game.LedSprite = null
+let mouse: game.LedSprite = null
 let cat = game.createSprite(0, 0)
 mouse = game.createSprite(4, 4)
 mouse.set(LedSpriteProperty.Brightness, 100)
 game.setScore(0)
-game.setLife(1)
+game.setLife(5)
 cat.change(LedSpriteProperty.Direction, 45)
 mouse.change(LedSpriteProperty.Direction, 270)
-basic.forever(function on_forever() {
+basic.forever(function () {
     cat.ifOnEdgeBounce()
     cat.move(1)
     if (cat.isTouching(mouse)) {
         game.removeLife(1)
     }
-    
     basic.pause(1000)
 })
